fdf4c86c98661485b8410d2d10a77e097910dac2
jhstarr <jeffhstarr@gmail.com>
2017-10-01 15:15:47 -0700

    Chapterv 1 Java Lab 1 Finished

---
+++ Hello.java
@@ -0,0 +1,22 @@
+// **********************************************************
+//  Hello.java
+//  Author: A. Starr
+//  Date: 10/1/17
+//  Purpose: Print a Hello, World message.
+//***********************************************************
+
+public class Hello
+{
+    //------------------------------------
+    //main methos -- prints the greetihng
+    //------------------------------------
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello, World!");
+    }
+}
+// 1. When I delete the "l", there is no error message, but I've noticed it automatically changes the visible name I see on the main folder for Lab 1.
+// 2. There is no error message when I take out an "l" in "Hello, World!" within the print statement because that can be anything you want it to be that you are telling the computer to type. When run, it now prints "Helo, World!".
+// 3. When you take out the ending quotation mark, it does not complie and says "unclosed string literal" as well as "reached the end of file while parsing".
+// 4. I get four errors without the beginning quotation mark.
+// 5. I get the message "';' expected".
--- Simple.java
+++ Simple.java
@@ -1,8 +1,8 @@
 // ********************************************
 //  Simple.java
-//
-//  Print a simple message about Java.
-//          
+//  Author: A. Starr
+//  Date: 10/1/17
+//  Purpose: Print a simple message about Java.
 // ********************************************
  
 public class Simple {
@@ -13,3 +13,10 @@ public class Simple {
  
     }
 }
+// 1. "simple" - This IS a legal identifier, but is not a good idea for a name because it does not begin with a  capital letter, which makes it difficult to differentiate from other lables.
+// 2. "SimpleProgram" - This IS a legal identifier and a good idea for a name.
+// 3. "1Simple" - This IS NOT a legal identifier because it begins with a digit (this will not compile).
+// 4. "_Simple_" - This IS a legal identifier, but not a very good idea because it is awkward with the underscores and doesn't simply begin with a capital letter.
+// 5. "*Simple*" - This IS NOT a legal identifier because it has two "*'s" (this will not compile).
+// 6. "$123_45" - This IS a legal identifier, but not a very good idea for a name because it is undescriptive.
+// 7. "Simple!" - This IS NOT a legal identifier because it has an "!" (this will not compile).
